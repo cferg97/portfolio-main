d8de5a5e5eb50d7069e5eeee1c5dc908d461ccc3
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2023-01-31 15:14:19 +0000

    start on about sec

--- src/components/AboutSec.jsx
+++ src/components/AboutSec.jsx
@@ -1,10 +1,39 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 const About = () => {
   return (
-    <Container fluid className="about-section">
+    <Container fluid className="about-section" id="about">
       <Row className="justify-content-center" style={{ height: "100%" }}>
-        <Col></Col>
+        <Col className="justify-content-center" style={{ height: "100%" }}>
+          <Container className="text-center mt-2">
+            <h1>About</h1>
+            <hr />
+            <Row style={{ height: "100%" }}>
+              <Col
+                md={4}
+                style={{ height: "100%" }}
+                className="justify-content-center"
+              >
+                <Container>
+                  <Image
+                    style={{ borderRadius: "100%", height: "15rem", marginRight: "1rem" }}
+                    
+                    src="https://res.cloudinary.com/cfcloudstorage/image/upload/v1675156396/media/avatar_uvtjvc.jpg"
+                    alt=""
+                  />
+                </Container>
+              </Col>
+              <Col className="text-center p-4">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Laudantium deleniti laborum debitis magni accusamus mollitia
+                  ea earum beatae rem! Obcaecati, explicabo. Nesciunt totam,
+                  nobis eius quaerat enim soluta incidunt libero.
+                </p>
+              </Col>
+            </Row>
+          </Container>
+        </Col>
       </Row>
     </Container>
   );
--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -14,7 +14,7 @@ const MyNav = () => {
           style={{ fontSize: "2rem", marginRight: "1rem", color: "#aaaaaa" }}
         /> */}
         <Nav>
-          <Nav.Link href="#home">About</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
           <Nav.Link disabled>|</Nav.Link>
           <Nav.Link href="#link">Skills</Nav.Link>
           <Nav.Link disabled>|</Nav.Link>
